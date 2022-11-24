c2a69a15c0ccef739d81a99508b65d0f461d3144
Lans12345 <olanalans12345@gmail.com>
2022-11-24 14:09:32 +0800

    added data table

--- lib/screens/pages/interview_history_page.dart
+++ lib/screens/pages/interview_history_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../widgets/app_bar.dart';
 import '../../widgets/drawer_widget.dart';
+import '../../widgets/text_widget.dart';
 
 class InterviewHistoryPage extends StatefulWidget {
   const InterviewHistoryPage({Key? key}) : super(key: key);
@@ -14,8 +15,41 @@ class _DashboardPageState extends State<InterviewHistoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-        drawer: const DrawerWidget(),
-        appBar: AppbarWidget('Interview History'));
+      backgroundColor: Colors.grey[200],
+      drawer: const DrawerWidget(),
+      appBar: AppbarWidget('Interview History'),
+      body: SingleChildScrollView(
+        child: DataTable(
+            // datatable widget
+            columns: [
+              // column to set the name
+              DataColumn(
+                  label: TextBold(
+                      text: 'Date', fontSize: 16, color: Colors.black)),
+              DataColumn(
+                  label: TextBold(
+                      text: 'Interviewe', fontSize: 16, color: Colors.black)),
+              DataColumn(
+                  label: TextBold(
+                      text: 'Applicant', fontSize: 16, color: Colors.black)),
+            ], rows: [
+          // row to set the values
+          for (int i = 0; i < 50; i++)
+            DataRow(cells: [
+              DataCell(
+                TextRegular(
+                    text: i.toString(), fontSize: 14, color: Colors.grey),
+              ),
+              DataCell(
+                TextRegular(
+                    text: 'Lance Olana', fontSize: 14, color: Colors.grey),
+              ),
+              DataCell(
+                TextRegular(text: 'John Doe', fontSize: 14, color: Colors.grey),
+              ),
+            ])
+        ]),
+      ),
+    );
   }
 }
--- lib/screens/pages/users_page.dart
+++ lib/screens/pages/users_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nat_corp_admin/widgets/text_widget.dart';
 
 import '../../widgets/app_bar.dart';
 import '../../widgets/drawer_widget.dart';
@@ -16,6 +17,41 @@ class _DashboardPageState extends State<UsersPage> {
     return Scaffold(
         backgroundColor: Colors.grey[200],
         drawer: const DrawerWidget(),
-        appBar: AppbarWidget('Users'));
+        appBar: AppbarWidget('Users'),
+        body: SingleChildScrollView(
+          child: DataTable(
+              // datatable widget
+              columns: [
+                // column to set the name
+                DataColumn(
+                    label: TextBold(
+                        text: 'No.', fontSize: 16, color: Colors.black)),
+                DataColumn(
+                    label: TextBold(
+                        text: 'Name', fontSize: 16, color: Colors.black)),
+                DataColumn(
+                    label: TextBold(
+                        text: 'Email', fontSize: 16, color: Colors.black)),
+              ], rows: [
+            // row to set the values
+            for (int i = 0; i < 50; i++)
+              DataRow(cells: [
+                DataCell(
+                  TextRegular(
+                      text: i.toString(), fontSize: 14, color: Colors.grey),
+                ),
+                DataCell(
+                  TextRegular(
+                      text: 'Lance Olana', fontSize: 14, color: Colors.grey),
+                ),
+                DataCell(
+                  TextRegular(
+                      text: 'olana@gmail.com',
+                      fontSize: 14,
+                      color: Colors.grey),
+                ),
+              ])
+          ]),
+        ));
   }
 }
