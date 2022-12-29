9322919aad1892456b2e8933c0e71437162aacb9
Lans12345 <olanalans12345@gmail.com>
2022-12-29 14:15:38 +0800

    populated data

--- lib/screens/pages/applicants_page.dart
+++ lib/screens/pages/applicants_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:nat_corp_admin/widgets/text_widget.dart';
 
@@ -18,57 +19,87 @@ class _DashboardPageState extends State<ApplicantsPage> {
       backgroundColor: Colors.grey[200],
       drawer: const DrawerWidget(),
       appBar: AppbarWidget('Applicants'),
-      body: ListView.builder(itemBuilder: ((context, index) {
-        return Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Container(
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10), color: Colors.white),
-            child: Row(children: [
-              Container(
-                child: const Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: CircleAvatar(
-                    minRadius: 25,
-                    maxRadius: 25,
-                    backgroundImage: AssetImage('assets/images/profile.png'),
-                  ),
-                ),
-                height: 100,
-                width: 100,
-                color: Colors.grey,
-              ),
-              const SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  TextBold(
-                      text: 'Name: John Doe',
-                      fontSize: 14,
-                      color: Colors.black),
-                  TextRegular(
-                      text: 'Contact Number: 09090104355',
-                      fontSize: 12,
-                      color: Colors.grey),
-                  TextRegular(
-                      text: 'Email Address: john@gmail.com',
-                      fontSize: 12,
-                      color: Colors.grey),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  TextBold(
-                      text: 'Preffered Job: Office Clerk',
-                      fontSize: 12,
-                      color: Colors.black),
-                ],
-              ),
-            ]),
-          ),
-        );
-      })),
+      body: StreamBuilder<QuerySnapshot>(
+          stream:
+              FirebaseFirestore.instance.collection('Applications').snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              print(snapshot.error);
+              return const Center(child: Text('Error'));
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              print('waiting');
+              return const Padding(
+                padding: EdgeInsets.only(top: 50),
+                child: Center(
+                    child: CircularProgressIndicator(
+                  color: Colors.black,
+                )),
+              );
+            }
+
+            final data = snapshot.requireData;
+            return ListView.builder(
+                itemCount: snapshot.data!.size,
+                itemBuilder: ((context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white),
+                      child: Row(children: [
+                        Container(
+                          child: const Padding(
+                            padding: EdgeInsets.all(10.0),
+                            child: CircleAvatar(
+                              minRadius: 25,
+                              maxRadius: 25,
+                              backgroundImage:
+                                  AssetImage('assets/images/profile.png'),
+                            ),
+                          ),
+                          height: 100,
+                          width: 100,
+                          color: Colors.grey,
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextBold(
+                                text:
+                                    'Name: ${data.docs[index]['firstName'] + ' ' + data.docs[index]['lastName']}',
+                                fontSize: 14,
+                                color: Colors.black),
+                            TextRegular(
+                                text:
+                                    'Contact Number: ${data.docs[index]['contactNumber']}',
+                                fontSize: 12,
+                                color: Colors.grey),
+                            TextRegular(
+                                text:
+                                    'Email Address: ${data.docs[index]['email']}',
+                                fontSize: 12,
+                                color: Colors.grey),
+                            const SizedBox(
+                              height: 5,
+                            ),
+                            TextBold(
+                                text:
+                                    'Preffered Job: ${data.docs[index]['positionDesired']}',
+                                fontSize: 12,
+                                color: Colors.black),
+                          ],
+                        ),
+                      ]),
+                    ),
+                  );
+                }));
+          }),
     );
   }
 }
