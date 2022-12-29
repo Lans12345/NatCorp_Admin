c9bb01edf49dc08f19b17eff7446f6d7c03d3eeb
Lans12345 <olanalans12345@gmail.com>
2022-12-29 18:57:46 +0800

    added interview func

--- lib/screens/pages/applicants_page.dart
+++ lib/screens/pages/applicants_page.dart
@@ -1,5 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:date_time_picker/date_time_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:nat_corp_admin/services/repositories/add_interview.dart';
 import 'package:nat_corp_admin/widgets/button_widget.dart';
 import 'package:nat_corp_admin/widgets/text_widget.dart';
 
@@ -14,6 +17,9 @@ class ApplicantsPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<ApplicantsPage> {
+  late String interviewCode = '';
+
+  late String dateAndTime = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,8 +76,140 @@ class _DashboardPageState extends State<ApplicantsPage> {
                                                         MainAxisAlignment
                                                             .center,
                                                     children: [
+                                                      TextBold(
+                                                          text:
+                                                              'Interview Details',
+                                                          fontSize: 18,
+                                                          color: Colors.black),
+                                                      Padding(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                    .fromLTRB(
+                                                                20, 10, 20, 20),
+                                                        child: TextFormField(
+                                                          decoration:
+                                                              InputDecoration(
+                                                            label: TextBold(
+                                                                text:
+                                                                    'Interview Code',
+                                                                fontSize: 12,
+                                                                color: Colors
+                                                                    .grey),
+                                                          ),
+                                                          onChanged: (_input) {
+                                                            interviewCode =
+                                                                _input;
+                                                          },
+                                                        ),
+                                                      ),
+                                                      Padding(
+                                                        padding:
+                                                            const EdgeInsets
+                                                                    .only(
+                                                                left: 10,
+                                                                right: 10,
+                                                                bottom: 20),
+                                                        child: Center(
+                                                          child: SizedBox(
+                                                            height: 50,
+                                                            width: 300,
+                                                            child:
+                                                                DateTimePicker(
+                                                                    type: DateTimePickerType
+                                                                        .dateTimeSeparate,
+                                                                    dateMask:
+                                                                        'd MMM, yyyy',
+                                                                    initialValue:
+                                                                        DateTime.now()
+                                                                            .toString(),
+                                                                    firstDate:
+                                                                        DateTime(
+                                                                            2000),
+                                                                    lastDate: DateTime(
+                                                                        2100),
+                                                                    icon: const Icon(Icons
+                                                                        .event),
+                                                                    dateLabelText:
+                                                                        'Intetview Date',
+                                                                    timeLabelText:
+                                                                        "Intetview Time",
+                                                                    onChanged:
+                                                                        (val) {
+                                                                      dateAndTime =
+                                                                          val;
+                                                                    },
+                                                                    validator:
+                                                                        (val) {
+                                                                      dateAndTime =
+                                                                          val!;
+                                                                      return null;
+                                                                    },
+                                                                    onSaved:
+                                                                        (val) {
+                                                                      dateAndTime =
+                                                                          val!;
+                                                                    }),
+                                                          ),
+                                                        ),
+                                                      ),
                                                       ButtonWidget(
-                                                          onPressed: () {},
+                                                          onPressed: () {
+                                                            addInterview(
+                                                                data.docs[index]
+                                                                    [
+                                                                    'companyName'],
+                                                                data.docs[index]
+                                                                    [
+                                                                    'companyLogo'],
+                                                                data.docs[index]
+                                                                    [
+                                                                    'companyId'],
+                                                                '',
+                                                                data.docs[index]
+                                                                    [
+                                                                    'positionDesired'],
+                                                                data.docs[index]
+                                                                    [
+                                                                    'companyName'],
+                                                                data.docs[index]
+                                                                        [
+                                                                        'firstName'] +
+                                                                    ' ' +
+                                                                    data.docs[index]
+                                                                        [
+                                                                        'lastName'],
+                                                                data.docs[index]
+                                                                    .id, // change to userId
+                                                                interviewCode,
+                                                                data.docs[index]
+                                                                    ['profile'],
+                                                                dateAndTime);
+
+                                                            FirebaseFirestore
+                                                                .instance
+                                                                .collection(
+                                                                    'Applications')
+                                                                .doc(data
+                                                                    .docs[index]
+                                                                    .id)
+                                                                .update({
+                                                              'status':
+                                                                  'Accepted'
+                                                            });
+
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop();
+                                                            Navigator.of(
+                                                                    context)
+                                                                .pop();
+
+                                                            Fluttertoast.showToast(
+                                                                toastLength: Toast
+                                                                    .LENGTH_LONG,
+                                                                msg:
+                                                                    'Interview Code - $interviewCode\nSchedule: $dateAndTime');
+                                                          },
                                                           text: 'Continue'),
                                                     ],
                                                   ),
--- lib/services/repositories/add_interview.dart
+++ lib/services/repositories/add_interview.dart
@@ -1,17 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future addInterview(
-  String companyName,
-  String companyLogo,
-  String companyId,
-  String positionDetails,
-  String position,
-  String interviewe,
-  String userName,
-  String uid,
-  String interviewCode,
-  String userProfilePic,
-) async {
+    String companyName,
+    String companyLogo,
+    String companyId,
+    String positionDetails,
+    String position,
+    String interviewe,
+    String userName,
+    String uid,
+    String interviewCode,
+    String userProfilePic,
+    String sched) async {
   final docUser = FirebaseFirestore.instance.collection('Interviews').doc();
 
   var dt = DateTime.now();
@@ -31,6 +32,8 @@ Future addInterview(
     'dateTime': DateTime.now(),
     'type': 'Ongoing',
     'date': dt.month,
+    'sched': sched,
+    'myId': FirebaseAuth.instance.currentUser!.uid
   };
 
   await docUser.set(json);
