d688731b4e8f2776652ea9aad80e02265d190bbd
Lans12345 <olanalans12345@gmail.com>
2022-12-29 09:10:54 +0800

    added delete feature

--- lib/screens/pages/companies_page.dart
+++ lib/screens/pages/companies_page.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:nat_corp_admin/screens/pages/add_company_screen.dart';
 import 'package:nat_corp_admin/utils/colors.dart';
 import 'package:nat_corp_admin/widgets/text_widget.dart';
@@ -57,51 +58,81 @@ class _DashboardPageState extends State<CompaniesPage> {
                 itemBuilder: ((context, index) {
                   return Padding(
                     padding: const EdgeInsets.all(5.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.white),
-                      child: Row(children: [
-                        Container(
-                          child: Padding(
-                            padding: const EdgeInsets.all(10.0),
-                            child:
-                                Image.network(data.docs[index]['companyLogo']),
-                          ),
-                          height: 100,
-                          width: 100,
-                          color: Colors.grey,
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            TextBold(
-                                text:
-                                    'Name: ${data.docs[index]['companyName']}',
-                                fontSize: 14,
-                                color: Colors.black),
-                            TextRegular(
-                                text:
-                                    'Address: ${data.docs[index]['companyAddress']}',
-                                fontSize: 12,
-                                color: Colors.grey),
-                            const SizedBox(
-                              height: 5,
+                    child: GestureDetector(
+                      onTap: (() {
+                        showModalBottomSheet(
+                            context: context,
+                            builder: (context) {
+                              return SizedBox(
+                                height: 100,
+                                child: ListTile(
+                                  onTap: () {
+                                    FirebaseFirestore.instance
+                                        .collection('Company')
+                                        .doc(data.docs[index].id)
+                                        .delete();
+                                    Fluttertoast.showToast(
+                                        msg: 'Deleted Succesfully!');
+                                    Navigator.of(context).pop();
+                                  },
+                                  leading: TextRegular(
+                                      text: 'Delete',
+                                      fontSize: 16,
+                                      color: Colors.red),
+                                  trailing: Icon(
+                                    Icons.delete,
+                                    color: Colors.red,
+                                  ),
+                                ),
+                              );
+                            });
+                      }),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: Colors.white),
+                        child: Row(children: [
+                          Container(
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Image.network(
+                                  data.docs[index]['companyLogo']),
                             ),
-                            TextBold(
-                                text: 'Available positions',
-                                fontSize: 12,
-                                color: Colors.black),
-                            TextRegular(
-                                text: '${data.docs[index]['position']}',
-                                fontSize: 10,
-                                color: Colors.grey),
-                          ],
-                        ),
-                      ]),
+                            height: 100,
+                            width: 100,
+                            color: Colors.grey,
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextBold(
+                                  text:
+                                      'Name: ${data.docs[index]['companyName']}',
+                                  fontSize: 14,
+                                  color: Colors.black),
+                              TextRegular(
+                                  text:
+                                      'Address: ${data.docs[index]['companyAddress']}',
+                                  fontSize: 12,
+                                  color: Colors.grey),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              TextBold(
+                                  text: 'Available positions',
+                                  fontSize: 12,
+                                  color: Colors.black),
+                              TextRegular(
+                                  text: '${data.docs[index]['position']}',
+                                  fontSize: 10,
+                                  color: Colors.grey),
+                            ],
+                          ),
+                        ]),
+                      ),
                     ),
                   );
                 }));
